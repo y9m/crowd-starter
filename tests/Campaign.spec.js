28e2923f2765693f7ac67e4c14ea27a10b90b27c
Jim Yang <jyang_123@hotmail.com>
2018-11-19 15:34:29 -0500

    added tests for owner address and minimum requirement

--- tests/Campaign.spec.js
+++ tests/Campaign.spec.js
@@ -35,4 +35,20 @@ describe('Campaigns', () => {
     expect(factory.options.address).to.not.equal(null);
     expect(campaign.options.address).to.not.equal(null);
   });
+
+  it('marks the creator as the campaign owner', async () => {
+    const owner = await campaign.methods.owner().call();
+    expect(owner).to.equal(accounts[0]);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: '50',
+        from: accounts[1]
+      });
+    } catch (err) {
+      expect(err).to.not.equal(null);
+    }
+  });
 });
