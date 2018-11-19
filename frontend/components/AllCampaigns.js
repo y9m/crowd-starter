ae4eaf17ebb12e843d28bfba5f93a628e45c2d9f
Jim Yang <jyang_123@hotmail.com>
2018-11-19 16:30:54 -0500

    added all campaigns page

--- frontend/components/AllCampaigns.js
+++ frontend/components/AllCampaigns.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import factory from '../../ethereum/factory';
+import { Card, Button } from 'semantic-ui-react';
 import { Layout } from '../components';
 
 class AllCampaigns extends Component {
+  state = {
+    campaigns: []
+  };
+
+  async componentDidMount() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    this.setState({ campaigns });
+  }
+
+  renderCampaigns() {
+    const { campaigns } = this.state;
+    const items = campaigns.map(address => {
+      return {
+        header: address,
+        description: <a href={`campaigns/${address}`}>View Campaign</a>,
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
-        <h1>List of Campaigns</h1>
+        <h3>Open Campaigns</h3>
+        <a href="campaigns/new">
+          <Button
+            floated="right"
+            content="Create Campaign"
+            icon="add circle"
+            primary
+          />
+        </a>
+        {this.renderCampaigns()}
       </Layout>
     );
   }
